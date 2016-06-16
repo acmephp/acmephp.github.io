5c7969804100eab7ef633f8d787741ed145f40fd
Titouan Galopin <galopintitouan@gmail.com>
2016-06-16 23:37:17 +0200

    Update theme and links for documentation

--- min/app.js
+++ min/app.js
@@ -1,2 +1,2 @@
-/*! Acme PHP | (c) Titouan Galopin | Build 20160615 */
+/*! Acme PHP | (c) Titouan Galopin | Build 20160616 */
 var App=function(r){function t(o){if(e[o])return e[o].exports;var n=e[o]={exports:{},id:o,loaded:!1};return r[o].call(n.exports,n,n.exports,t),n.loaded=!0,n.exports}var e={};return t.m=r,t.c=e,t.p="",t(0)}([function(r,t){"use strict"}]);
